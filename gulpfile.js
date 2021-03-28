a652e6b666a5a70bea22a70b262b7ce5598768b9
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-03-28 13:05:07 +0100

    Added paths for workflow automation

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,21 @@ const minfiy = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass')(require('sass'));
 
+const paths = {
+	src: {
+		css: './assets/css/src/**/*.scss'
+	},
+	dist: {
+		css: './assets/css/dist'
+	}
+}
+
 const compile = () => {
-	return src()
+	return src(paths.src.css)
 		.pipe(sass())
 		.pipe(autoprefixer())
 		.pipe(minify())
-		.pipe(dest());
+		.pipe(dest(paths.dist.css));
 }
+
+exports.sass = compile;
