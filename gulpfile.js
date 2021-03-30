f4c1b7bae086c8833e72861ebe2d35ccfc916b8e
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-03-30 13:09:34 +0100

    Fixed typo error on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const { src, dest, watch } = require('gulp');
 
-const minfiy = require('gulp-clean-css');
+const minify = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass')(require('sass'));
 
