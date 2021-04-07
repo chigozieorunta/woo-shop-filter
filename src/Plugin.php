1d5a1278c0425bfe1bf0f23cda6383635d881c4c
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-04-07 13:36:29 +0100

    Defined main plugin file

---
+++ src/Plugin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin class.
+ *
+ * @package WooShopFilter
+ */
+
+namespace WooShopFilter;
+
+/**
+ * WordPress plugin interface.
+ */
+class Plugin {
+
+	/**
+	 * Plugin's singleton instance
+	 *
+	 * @var object
+	 */
+	private static $instance;
+
+	/**
+	 * Plugin Entry point based on Singleton
+	 *
+	 * @return Plugin $plugin Instance of the plugin abstraction.
+	 */
+	public static function init() {
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+}
