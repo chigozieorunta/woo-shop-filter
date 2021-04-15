4f4c8efbe649d75fbeb5334eac33f6cc0e0ac74d
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-04-15 14:03:57 +0100

    Registered css for plugin display

--- src/Plugin.php
+++ src/Plugin.php
@@ -20,6 +20,24 @@ class Plugin {
 	private static $instance;
 
 	/**
+	 * Setup the plugin.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_css' ] );
+	}
+
+	/**
+	 * Enqueue CSS method
+	 *
+	 * @return void
+	 */
+	public function register_css() {
+		wp_enqueue_style( 'woo-shop-filter', plugin_dir_url( __FILE__ ) . '../assets/css/dist/plugin.css' );
+	}
+
+	/**
 	 * Plugin Entry point based on Singleton
 	 *
 	 * @return Plugin $plugin Instance of the plugin abstraction.
